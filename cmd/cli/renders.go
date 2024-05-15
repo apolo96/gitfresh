1e76fc65d973ea20a0016d4173bab0c754411769
Andres Polo <andresfpvclap@gmail.com>
2024-05-15 12:37:18 -0500

    feat: Update renderRepos function to include owner and conditional hook URL rendering (#2)

--- cmd/cli/renders.go
+++ cmd/cli/renders.go
@@ -6,13 +6,20 @@ import (
 	"github.com/apolo96/gitfresh"
 )
 
+var Verbose bool
+
 func renderVerbose(text string) {
-	println(text)
+	if Verbose {
+		println(text)
+	}
 }
 
-func renderRepos(rp []*gitfresh.Repository) {
+func renderRepos(rp []*gitfresh.Repository, fresh bool) {
 	for _, r := range rp {
-		url := fmt.Sprintf("https://github.com/apolo96/%s/settings/hooks", r.Name)
-		fmt.Printf("Repository: %-30s | URL: %-20s\n", r.Name, url)
+		url := fmt.Sprintf("https://github.com/%s/%s", r.Owner, r.Name)
+		if fresh {
+			url = url + "/settings/hooks"
+		}
+		fmt.Printf("Repository: %-25s | URL: %-20s\n", r.Name, url)
 	}
 }
